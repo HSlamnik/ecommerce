3b088163e88fd4a151d8d13b24691929a70ba784
Haris Slamnik <haris94@hotmail.it>
2021-01-27 19:46:02 +0000

    Created layout for shopping pages

---
+++ src/utils/constants.js
@@ -0,0 +1,30 @@
+export const SHOP_SECTIONS = [{
+    id: 1,
+    title: 'HATS',
+    subtitle: 'SHOP NOW',
+    isWideDiv: true
+},
+{
+    id: 2,
+    title: 'JACKETS',
+    subtitle: 'SHOP NOW',
+    isWideDiv: true
+},
+{
+    id: 3,
+    title: 'SNEAKERS',
+    subtitle: 'SHOP NOW',
+    isWideDiv: true
+},
+{
+    id: 4,
+    title: 'WOMENS',
+    subtitle: 'SHOP NOW',
+    isWideDiv: true
+},
+{
+    id: 5,
+    title: 'MENS',
+    subtitle: 'SHOP NOW',
+    isWideDiv: true
+},]
